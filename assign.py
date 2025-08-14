16f6fcc4b65baad293f269bdfee3a831a329206c
Njien <thomasjessyendi@gmail.com>
2025-08-14 18:03:00 +0100

    first commit

---
+++ assign.py
@@ -0,0 +1,73 @@
+ussd = "*555#"
+balance = 1000
+#phone_number = int(input("Enter phone number:\n>>> "))
+#enter_ussd = input("Enter USSD to continue:\n>>> ")
+SHOULD_EXECUTE = True
+while SHOULD_EXECUTE:
+	balance = 1000
+	phone_number = int(input("Enter phone number:\n>>> "))
+	enter_ussd = str(input("Enter USSD to continue:\n>>> "))
+	if enter_ussd == ussd:
+		options = ["buy data", "buy airtime", "check balance", "exit"]
+
+		print("""AVAILABLE OPTIONS:
+		1. Buy Data
+		2. Buy airtime
+		3. Check balance
+		4. Exit
+		""")
+		enter_choice = int(input("Enter choice from the above options:\n>>> "))
+
+		if enter_choice == 1:
+			print(options[0])
+			data_plan = {"1gb": 300, "2GB": 600, "3.5GB":  1000}
+			print("""AVAILABLE PLANS:
+			1. 1GB @ 300
+			2. 2GB @ 600
+			3. 3.5GB @ 1000""")
+			data_balance = int(input("enter choice from the above:\n>>> "))
+
+			if data_balance == 1:
+				if  data_plan["1gb"] <= balance:
+					
+					print(f"You have successfully purchase 1gb on {Phone_number}\nThank you for choosing MTN")
+					new_balance = balance - data_balance
+					print(f"Your remaing balance is: {new_balance}")
+				else:
+					print(f"insufficient balance:\nAvaialble balance is:{balance}")
+
+			elif  data_balance == 2:
+				if  data_plan["2GB"] <= balance:
+					print(f"You have successfully purchase 2gb on {Phone_number}\nThank you for choosing MTN")
+					new_balance = balance - data_balance
+					print(f"Your remaing balance is: {new_balance}")
+				else:
+					print(f"insufficient balance:\nAvaialble balance is:{balance}")
+			elif data_balance == 3:
+				if  data_plan["3.5GB"] <= balance:
+					print(f"You have successfully purchase 3gb on {Phone_number}\nThank you for choosing MTN")
+					new_balance = balance - data_balance
+					print(f"Your remaing balance is: {new_balance}")
+
+			else:
+				print("invalid options")
+		elif enter_choice == 2:
+			print(options[1])
+			
+			airtime_amount = int(input("Enter amount"))
+			if airtime_amount <= balance:
+				 print(f"You have successfully purchase airtime of {airtime_amount} on {'Phone_number'}\nThank you for choosing MTN")
+				 new_balance = balance - airtime_amount
+				 print(f"Your remaing balance is: {new_balance}")
+			else:
+				 print(f"insufficient balance:\nAvaialble balance is:{balance}")
+		elif choice == 3:
+			print(options[2])
+
+			print(f"Your Available balance is: {balance}")
+		elif choice == 4:
+			SHOULD_EXECUTE = False
+		else:
+			print("invalid option..\nTry againlater")
+	else:
+		print("invalid ussd")
